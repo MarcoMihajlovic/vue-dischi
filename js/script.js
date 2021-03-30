3d5534b2a75613904e227021d8dff5f51287a27e
Marco <marcosinisa.mihajlovic@gmail.com>
2021-03-30 16:53:51 +0200

    Bonus 1 completato

--- js/script.js
+++ js/script.js
@@ -3,16 +3,35 @@ var app = new Vue ({
     
     data: {
         albums: [],
+        types: [],
+        filterGenre:"all"
 
+    },
+
+    methods: {
+        getAlbums: function() {
+            if(this.filterGenre == 'all') {
+                return this.albums;
+            }
+                return this.albums.filter(element => element.genre == this.filterGenre)
+        }
     },
 
     created () {
             axios
                 .get('https://flynn.boolean.careers/exercises/api/array/music')
                 .then((request) => {
-                    request.data.response.forEach(element => this.albums.push(element))
-                 this.albums.sort((a, b) => (a.year > b.year) ? 1 : -1)  
-                })
-                console.log(this.albums);
+                    request.data.response.sort((a, b) => (a.year > b.year) ? 1 : -1);
+                    request.data.response.forEach(element => {
+                        this.albums.push(element)
+                        if(!this.types.includes(element.genre)) {
+                            this.types.push(element.genre);
+                        };
+                    })
+                
+            
+            });
+            console.log(this.albums);
+            Vue.config.devtools = true;
     }
 })
